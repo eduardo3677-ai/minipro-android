abb7d4854bb62b4bb8b6bc13394b166ef8414f85
David Griffith <dave@661.org>
2025-08-03 21:30:26 -0700

    Updated T48 latest firmware (01.1.34 / 0x122)

--- src/t48.h
+++ src/t48.h
@@ -18,8 +18,8 @@
 #ifndef __T48_H
 #define __T48_H
 
-#define T48_FIRMWARE_VERSION 0x120
-#define T48_FIRMWARE_STRING  "01.1.32"
+#define T48_FIRMWARE_VERSION 0x122
+#define T48_FIRMWARE_STRING  "01.1.34"
 
 /* T48 low level functions. */
 int t48_begin_transaction(minipro_handle_t *handle);
