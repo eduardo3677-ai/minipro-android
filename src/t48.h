f124338492a3937dbf02f9c4bc5219892f10481e
David Griffith <dave@661.org>
2025-09-24 14:56:36 -0700

    Updated known latest firmwares for T48 and T76.
    T48 (01.1.35 | 12.96 | Aug  4, 2025 | 0x123)
    T76 (01.1.13 | 13.03 | Sep 15, 2025 | 0x10D)

--- src/t48.h
+++ src/t48.h
@@ -18,8 +18,8 @@
 #ifndef __T48_H
 #define __T48_H
 
-#define T48_FIRMWARE_VERSION 0x122
-#define T48_FIRMWARE_STRING  "01.1.34"
+#define T48_FIRMWARE_VERSION 0x123
+#define T48_FIRMWARE_STRING  "01.1.35"
 
 /* T48 low level functions. */
 int t48_begin_transaction(minipro_handle_t *handle);
--- src/t76.h
+++ src/t76.h
@@ -18,8 +18,8 @@
 #ifndef __T76_H
 #define __T76_H
 
-#define T76_FIRMWARE_VERSION 0x109
-#define T76_FIRMWARE_STRING  "00.1.09"
+#define T76_FIRMWARE_VERSION 0x10D
+#define T76_FIRMWARE_STRING  "00.1.13"
 
 /* T76 low level functions. */
 int t76_begin_transaction(minipro_handle_t *handle);
